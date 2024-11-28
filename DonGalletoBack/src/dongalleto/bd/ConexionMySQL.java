346dc40f0bbc773bffbd918ebe80eef06f74b379
EduardoBalderas <balderas.hector09@gmail.com>
2024-11-28 13:21:57 -0600

    Se agrego paquete

---
+++ DonGalletoBack/src/dongalleto/bd/ConexionMySQL.java
@@ -0,0 +1,28 @@
+package dongalleto.bd;
+
+import java.sql.Connection;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionMySQL {
+
+    String url = "jdbc:mysql://localhost:3306/don_galleto";
+    String user = "root";
+    String password = "root";
+
+    public Connection abrirConexion() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection conexion = DriverManager.getConnection(url, user, password);
+
+        return conexion;
+    }
+
+    public void cerrarConexion(Connection conexion) throws IOException, SQLException {
+        if(conexion!=null){
+            conexion.close();
+        }
+        
+    }
+}
